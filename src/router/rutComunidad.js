67ad48d16ce1cd45a4759e247a0f54ea787a2653
RominaDiaz01 <romiidiaz06@gmail.com>
2022-08-26 18:45:41 -0300

    cambios

--- src/router/rutComunidad.js
+++ src/router/rutComunidad.js
@@ -3,10 +3,6 @@ const router = express.Router()
 const path = require("path")
 
 
-router.get("/temasnuevos",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"../views/temasnuevos.html"))
-})
-
 router.get("/temasactivos",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"../views/temasactivos.html"))
 })
