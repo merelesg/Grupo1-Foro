81509ef9a93f1535f6e58144d2a63354af435db9
Agustin Mereles <78210644+merelesg@users.noreply.github.com>
2022-09-05 01:58:52 -0300

    Add files via upload

--- public/js/registro.js
+++ public/js/registro.js
@@ -1,18 +1,15 @@
-const nombre = document.getElementById("name")
-const email = document.getElementById("email")
-const password = document.getElementById("password")
-const form = document.getElementById("form")
-const paraffo = document.getElementById("warnings")
+const nombre = document.getElementById("Nombre")
+const email = document.getElementById("Mail")
+const password = document.getElementById("Contraseña")
+const User = document.getElementById("Usuario")
 
 form.addEventListener("submit", e=>{
     e.preventDefault()
-    let warnings = ""
+    let warnings = "alert"
     let regexEmail= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     if(nombre.value.length <6){
         warnings += 'el nombre no es valido <br>'
     }
-    console.log("")
-{    if(regexEmail.test(email.value){
+    console.log(data)
 
-}
 }})
